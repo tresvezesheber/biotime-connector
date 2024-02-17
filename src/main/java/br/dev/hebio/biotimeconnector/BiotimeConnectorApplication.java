52c75cf4709a67127a5e3fc869008ac5c780bfe9
tresvezesheber <hebireaper@gmail.com>
2024-02-17 12:57:51 -0300

    its alive

---
+++ src/main/java/br/dev/hebio/biotimeconnector/BiotimeConnectorApplication.java
@@ -0,0 +1,13 @@
+package br.dev.hebio.biotimeconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiotimeConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiotimeConnectorApplication.class, args);
+	}
+
+}
