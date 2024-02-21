b5f7fc0b2986494af3fa10b2a1814f90a4ab0163
tresvezesheber <hebireaper@gmail.com>
2024-02-21 11:25:55 -0300

    feat: alter the conditions of 'defineAcesso' method

--- src/main/java/br/dev/hebio/biotimeconnector/service/ColaboradorService.java
+++ src/main/java/br/dev/hebio/biotimeconnector/service/ColaboradorService.java
@@ -39,7 +39,19 @@ public class ColaboradorService {
     }
 
     public static String defineAcesso(char colaboradorSituacao) {
-        if (!(colaboradorSituacao == 'A')) {
+        /*A - ATIVO, C - CONTRATO DE TRABALHO SUSPENSO, D - DEMITIDO, E - LICENÇA MATER., F - FÉRIAS, G - RECESSO REMUNERADO DE ESTAGIO, I - APOS. POR INCAPACIDADE PERMANENTE,
+        K - CESSÃO/REQUISIÇÃO, L - LICENÇA S/VENC, M - SERV. MILITAR, N - MANDATO SINDICAL ÔNUS DO EMPREGADOR, O - DOENÇA OCUPACIONAL, P - AF. PREVIDÊNCIA, Q - PRISÃO/CÁRCERE,
+                R - LICENÇA REMUN., S - MANDATO SINDICAL ÔNUS SINDICATO, T - AF. AC. TRABALHO, U - OUTROS, V - AVISO PRÉVIO, W - LICENÇA MATER. COMPL. 180 DIAS, X - C/DEM. NO MÊS,
+                Y - LICENÇA PATERNIDADE, Z - ADMISSÃO PROX. MÊS*/
+
+        if (!(colaboradorSituacao == 'A'
+                || colaboradorSituacao == 'E'
+                || colaboradorSituacao == 'F'
+                || colaboradorSituacao == 'L'
+                || colaboradorSituacao == 'R'
+                || colaboradorSituacao == 'V'
+                || colaboradorSituacao == 'W'
+                || colaboradorSituacao == 'Y')) {
             return "1";
         }
         return "0";
